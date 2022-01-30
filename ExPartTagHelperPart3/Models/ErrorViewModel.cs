6fbe1500c5c3860c47a8b70a3029cbb6b3298bbf
unknown <faezehjafarpour75@gmail.com>
2022-01-30 23:16:53 +0330

    TagHelper

---
+++ ExPartTagHelperPart3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExPartTagHelperPart3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
